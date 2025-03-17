6ba4f72526efc29cb2bd7d059df1f70e59301f81
Jegorik <jegorik@aroundacloud.co.uk>
2025-03-17 14:24:41 +0200

    code refactor

--- challenge-1/variables.tf
+++ challenge-1/variables.tf
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 variable "aws_region" {
-  type = string
+  type        = string
   description = "AWS Region"
-  default = "us-west-1"
-=======
-variable "instance_type" {
-  type        = string                     # The type of the variable, in this case a string
-  default     = "t2.micro"                 # Default value for the variable
-  description = "The type of EC2 instance" # Description of what this variable represents
->>>>>>> 989d4df218a8d7edfa96bf47a87cfbd109943b69
+  default     = "us-west-1"
 }
